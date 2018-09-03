7f42ede1025ab355fc630005c44573ef146158aa
Miguel <miguel_reyes_c@hotmail.com>
2018-09-02 22:15:23 -0300

    Outputting a List of Recipes with ngFor

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,9 +1,9 @@
-export class Recipe{
+export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
 
-    Recipe(name: string, desc: string, imagePath: string){
+    constructor(name: string, desc: string, imagePath: string) {
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
